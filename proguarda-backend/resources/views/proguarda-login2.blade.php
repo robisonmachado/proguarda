5b3df1856dc46137a53c49cc5e8f5e9ce47ad4e0
Robison Machado <robisonpmachado@gmail.com>
2019-03-09 04:45:09 -0300

    atualizada página de login provisória

--- proguarda-backend/resources/views/proguarda-login2.blade.php
+++ proguarda-backend/resources/views/proguarda-login2.blade.php
@@ -4,13 +4,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Jekyll v3.8.5">
-    <title>Signin Template · Bootstrap</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
-
+    <meta name="description" content="ProGuarda">
+    <meta name="author" content="Cristóvão dos Santos Bahiense e Robison Pereira Machado">
+   
+    <title>ProGuarda - Login</title>
 
     
 <link rel="stylesheet" href="css/app.css">
